72ee03f61fe96767b26e3ec496e17161809d7cb2
PedroScheffer <pscheffergoncalves@gmail.com>
2023-09-12 11:46:56 -0300

    update suporte mobile

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const shareButtons = document.querySelectorAll('.tile-share-button')
 console.log(shareButtons)
 
 async function copyText(e) {
-//prevent button going to the site
-    e.preventDefault()
-    const link = this.getAttribute('link')
-    console.log(link)
+    e.preventDefault();
+
+    const link = this.getAttribute('link');
+    console.log(link);
+
     try {
-        await navigator.clipboard.writeText(link)
-        alert("Link Copiado: "+ link)
+        await navigator.clipboard.writeText(link);
+        alert("Link Copiado: " + link);
     } catch (err) {
-        console.error(err)
+        console.error(err);
+        alert("Falha ao copiar o link.");
     }
 }
+
 
 shareButtons.forEach(shareButton =>
     shareButton.addEventListener('click', copyText))
