5e55e72423f26fa1ed64fdd622540bee78c6aebf
PedroScheffer <pscheffergoncalves@gmail.com>
2023-09-12 14:07:29 -0300

    update error link

--- app.js
+++ app.js
@@ -2,20 +2,17 @@ const shareButtons = document.querySelectorAll('.tile-share-button')
 console.log(shareButtons)
 
 async function copyText(e) {
-    e.preventDefault();
 
-    const link = this.getAttribute('link');
-    console.log(link);
-
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
     try {
-        await navigator.clipboard.writeText(link);
-        alert("Link Copiado: " + link);
+        await navigator.clipboard.writeText(link)
+        alert("Link Copiado: " + link)
     } catch (err) {
-        console.error(err);
-        alert("Falha ao copiar o link.");
+        console.error(err)
     }
 }
-
 
 shareButtons.forEach(shareButton =>
     shareButton.addEventListener('click', copyText))
